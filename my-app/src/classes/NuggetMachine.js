5e79b4f45302d5838c174b0b0eb53803d6509b55
Tyler Yih <tyih985@students.cs.ubc.ca>
2025-01-19 00:47:55 -0800

    Added images to the project for 64 Nuggetmon. Also implemented User.js and NuggetMachine.js

--- my-app/src/classes/NuggetMachine.js
+++ my-app/src/classes/NuggetMachine.js
@@ -1,22 +1,86 @@
-import {Nuggetmon} from "./Nuggetmon";
+import { Nuggetmon } from "./Nuggetmon";
 
 class NuggetMachine {
     constructor() {
-        this.nuggetmonPool = [
-            { name: "Sparkle", rarity: "Common" },
-            { name: "Glimmer", rarity: "Uncommon" },
-            { name: "Radiance", rarity: "Rare" },
-            { name: "Luminous", rarity: "Epic" },
-            { name: "Celestial", rarity: "Legendary" }
+        this.possibleNuggetmon = [
+            "Nugget",
+            "Kai",
+            "Chill guy",
+            "Kirby",
+            "Waddle Dee",
+            "Chao",
+            "Beeg Yoshi",
+            "Shy Guy",
+            "Kung Fu Dugong",
+            "Oshawott",
+            "Pikachu",
+            "Eevee",
+            "Vaporeon",
+            "Jeff the Land Shark",
+            "Kendrick Lamar vs Drake",
+            "Mickey Mouse",
+            "DJ Khaled",
+            "Chopper",
+            "Pochita",
+            "Winnie the Pooh",
+            "Hello Kitty",
+            "Teemo",
+            "Pikmin Trio",
+            "Rock Pikmin",
+            "Purple Pikmin",
+            "Bob the Builder",
+            "Blues Clues",
+            "The Squad",
+            "Baby Groot",
+            "Bob",
+            "Totoro",
+            "Katie",
+            "Tutter",
+            "Mike Wazowski",
+            "Grimace",
+            "Elmo",
+            "Cookie Monster",
+            "Bert and Ernie",
+            "Stewie",
+            "Peter",
+            "Ted",
+            "Jungkook",
+            "Miga",
+            "Quatchi",
+            "Sumi",
+            "Mukmuk",
+            "Cinnamoroll",
+            "Kuromi",
+            "Pompompurin",
+            "Gudetama",
+            "Badtz-Maru",
+            "Subway Surfers",
+            "Fire Boy and Water Girl",
+            "Om Nom",
+            "Talking Tom",
+            "Talking Ben",
+            "Among Us",
+            "Fall Guy",
+            "Pablo",
+            "Toopy and Binoo",
+            "Pusheen",
+            "Lightning McQueen",
+            "Goku",
+            "Lebron"
         ];
     }
-    rollNuggetmon(nuggets) {
-        const rollCost = 100;
-        if (nuggets.subtract(rollCost)) {
-            const randomIndex = Math.floor(Math.random() * this.nuggetmonPool.length);
-            const { name, rarity } = this.nuggetmonPool[randomIndex];
-            return new Nuggetmon(name, rarity);
+
+    roll(user) {
+        const rollCost = 120;
+        if (user.subtractNuggets(rollCost)) {
+            const randomIndex = Math.floor(Math.random() * this.possibleNuggetmon.length);
+            const nuggetmonName = this.possibleNuggetmon[randomIndex];
+            const newNuggetmon = new Nuggetmon(nuggetmonName, "", Date.now(), 0, 10, "");
+            user.addNuggetmon(newNuggetmon);
+        } else {
+            console.log("Not enough nuggets to roll.");
         }
-        return null;
     }
 }
+
+export default NuggetMachine;
