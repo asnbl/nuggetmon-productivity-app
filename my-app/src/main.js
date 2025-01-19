bbea342106b5b1c66343b85f17e79090bbbf06c2
ethan <ethanhuynh.younus@gmail.com>
2025-01-18 18:46:17 -0800

    Reverted changes

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -2,21 +2,17 @@ import { app, BrowserWindow } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit();
 }
 
-const { windowManager } = require("node-window-manager");
+let mainWindow;
+let popupWindow;
 
-// const window = windowManager.getActiveWindow();
-//
-// // Prints the currently focused window bounds.
-// console.log(window.getTitle());
+const {windowManager} = require('node-window-manager')
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -24,7 +20,6 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
@@ -43,7 +38,7 @@ const trackWindows = () => {
 }
 
 const getOpenWindows = () => {
-  const windows = getWindows();
+  const windows = windowManager.getWindows();
 
   // Filter to include only visible windows
   const visibleWindows = windows.filter(window => window.isVisible());
@@ -57,9 +52,6 @@ const getOpenWindows = () => {
   // console.log('Open Windows:', visibleWindows);
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
@@ -78,14 +70,8 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
