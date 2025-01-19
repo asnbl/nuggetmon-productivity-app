0332950ab120f774e29b469fb3251f434a4fe337
Angela Li <angela.li@outlook.com>
2025-01-19 01:40:41 -0800

    popup html

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -42,8 +42,8 @@ const createPopupWindow = () => {
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
